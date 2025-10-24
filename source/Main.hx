243c6a638b9c36870aae751a63a30f26247f3ced
GaboWuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2025-10-24 20:58:54 -0300

    real

--- source/Main.hx
+++ source/Main.hx
@@ -12,7 +12,6 @@ import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
-import webm.*;
 
 class Main extends Sprite
 {
@@ -49,8 +48,6 @@ class Main extends Sprite
 			addEventListener(Event.ADDED_TO_STAGE, init);
 		}
 	}
-
-	public static var webmHandler:WebmHandler;
 
 	private function init(?E:Event):Void
 	{
